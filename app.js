22cbeda48af715b6d0e500d7679386be37a3c89f
Samuel Čus <xcus@fi.muni.cz>
2026-05-03 14:07:18 +0200

    Add SSL to database connection for Azure

--- app.js
+++ app.js
@@ -36,6 +36,9 @@ const db = new pg.Client({
   database: process.env.DB_DATABASE,
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 db.connect();
 
